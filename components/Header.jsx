ca4b74a4793fa8f0c0619724b303682d72904979
NiayeshTorabi <niyayesht81@gmail.com>
2024-10-29 00:57:19 +0330

    Version 1.5.1

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,10 +8,10 @@ import MobileNav from "./MobileNav.jsx";
 const Header = () => {
   // py-8 xl:py-12
   return <header dir="ltr" className="py-2">
-      <div className="container mx-auto flex justify-between items-center justify-items-center justify-self-center content-center self-center">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
-          <h1 className="text-2xl justify-center items-center justify-items-center justify-self-center content-center self-center">
+          <h1 className="text-2xl mt-3">
             TechnoRaspberry
           </h1>
         </Link>
