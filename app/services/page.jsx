f5fe8b24585ffca5a915edc9da070ebdf7fb6c38
NiayeshTorabi <niyayesht81@gmail.com>
2024-11-07 18:15:58 +0330

    1.8.2.9

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -42,9 +42,13 @@ const Services = () => {
         </div>
           </div>
         {/* data */}
-        <div className="flex xl:flex-row flex-col-reverse justify-between xl:mr-[19rem] xl:ml-[10rem]">
-          <div className="container mx-auto text-center mt-16"><p className="font-bold text-xl leading-[2rem] mb-5 text-right">سلام دوست خوبم!</p><p className="text-justify text-xl leading-[2rem] xl:leading-[3rem]">مجموعه تکنورزبری یک مجموعه حرفه‌ای با ارائه خدمات حرفه‌ای است</p><p className="text-justify text-xl leading-[2rem] xl:leading-[3rem]">در ادامه می‌توانید خدمات ما را مشاهده کنید؛ برای جابه‌جایی بین اسلایدرها روی آنها کلیک کنید و آنها را انتخاب کرده، سپس می‌توانید با کلیک روی دکمه‌ی "مشاهده" که پایینِ هر بخش وجود دارد، نسبت به مشاهده جزئی‌تر آن خدمت نیز بپردازید؛ در صورت وجود هرگونه ابهام نیز از صفحه ارتباط با ما اقدام فرمایید.</p></div>
-        <img src="/images/services-main.jpg" className="h-[300px] xl:h-[375px] w-full object-contain" />
+        <div className="container mx-auto flex xl:flex-row flex-col-reverse w-[full]">
+          <div className="text-center mt-16 px-3">
+              <p className="font-bold text-xl leading-[2rem] mb-5 text-right">سلام دوست خوبم!</p>
+              <p className="text-justify text-xl leading-[2rem] xl:leading-[3rem]">مجموعه تکنورزبری یک مجموعه حرفه‌ای با ارائه خدمات حرفه‌ای است</p>
+              <p className="text-justify text-xl leading-[2rem] xl:leading-[3rem]">در ادامه می‌توانید خدمات ما را مشاهده کنید؛ برای جابه‌جایی بین اسلایدرها روی آنها کلیک کنید و آنها را انتخاب کرده، سپس می‌توانید با کلیک روی دکمه‌ی "مشاهده" که پایینِ هر بخش وجود دارد، نسبت به مشاهده جزئی‌تر آن خدمت نیز بپردازید؛ در صورت وجود هرگونه ابهام نیز از صفحه ارتباط با ما اقدام فرمایید.</p>
+          </div>
+        <img src="/images/services-technoraspberry.png" className="h-[300px] xl:h-[375px] w-[full] object-contain mix-blend-multiply" />
         </div>
         <p className="container mx-auto text-3xl text-center mt-20 font-bold text-accent xl:leading-[2rem] leading-[3rem]">اسلایدرها در زیر قابل مشاهده هستند !!</p>
           <div class="slider mt-5">
